c84bd6acbe2177dcf8ecd6afd8704cd0c02f007e
valooost <152898127+valooost@users.noreply.github.com>
2025-06-22 10:49:58 +0200

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,10 +1,20 @@
+#include <Servo.h>
+
 const int trigPin = 7;
 const int echoPin = 6;
+const int servoPin = 8;
+
+//A Variable to change the Servo's "Angle", to adjust your Switch's location
+int position = 80;
+
+Servo myServo;
 
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  myServo.attach(servoPin);
+  myServo.write(90);
 }
 
 void loop() {
@@ -16,15 +26,18 @@ void loop() {
 
   long duration = pulseIn(echoPin, HIGH);
   float distance = duration * 0.0343 / 2;
-
+  
   Serial.print("distance: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  delay(500);
-  
-  if (distance < 100) 
-  {
-    Serial.print("Now I would switch the light off  ");
+  if (distance < 100) {
+    Serial.println("Now I'll move the Servo");
+    myServo.write(position);
+    delay(500);
+    
   }
+
+  myServo.write(0); 
+  delay(500);
 }
